4f398ca1469965d6de935eaaf08af078d5c23bd6
richardessemiah <105212876+richardessemiah@users.noreply.github.com>
2024-09-12 09:55:09 +0200

    Initial Commit

---
+++ SprintTreeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SprintTreeProject
+//
+//  Created by Richard Essemiah on 12/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SprintTreeProject/SprintTreeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprintTreeProjectApp.swift
+//  SprintTreeProject
+//
+//  Created by Richard Essemiah on 12/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SprintTreeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
